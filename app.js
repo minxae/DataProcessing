2cda44b74bdbf1db60db0bff5e1f2959c010990a
Bram Jansen <39745649+minxae@users.noreply.github.com>
2022-03-22 16:07:33 +0100

    New schema for selecting one record from the database

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const res = require("express/lib/response");
 const app = express();
 const mysql = require("mysql"); //Mysql
 const Ajv = require("ajv"); //AVJ JSON schema validator
+const { query } = require("express");
 
 app.use(express.json());
-// -AJV schema lib
-const schema = require("./JSON_Schemas/JSON_schema_GNI_Male.json");
+// -AJV schema lib for female and male 
+const GNI_MaleFemale_schema = require("./JSON_Schemas/JSON_schema_GNI_Male.json");
+const GNI_MaleFemale_schema_single_record = require("./JSON_Schemas/JSON_schema_GNI_Male_single_record.json");
+const { resolveSchema } = require("ajv/dist/compile");
 
 const ajv = new Ajv();
 
@@ -36,39 +39,135 @@ app.get('/testRoute', (req, res) =>
     res.send("TESTING ROUTE");
 
 });
+conn.connect(function(err)
+{
+    if(err) throw err;
+});
 
 // -Selecting all data from table.
-app.post("/insertData", (req, res)=>
+// - This endpoint gets all the record that are the table:
+// # estimated_gni_male
+
+app.get("/estimatedGNIMale", (req, res)=>
 {
-    const validJson = ajv.validate(schema, req.body);
-    if(validJson)
+    const query = "SELECT * FROM estimated_gni_male"
+    
+    conn.query(query, function(err, result, fields)
     {
-        res.status(200)
-        res.send("DATA BEEN INSERTED");
+        if(result.length > 0)
+        {
+            if(err) throw err;
+
+            const valid = ajv.validate(GNI_MaleFemale_schema, result); 
+    
+            // -checks if JSON data is valid with the schema
+            if(valid)
+            {
+                res.status(200);
+                res.send(result);
+            }else
+            {
+                res.status(400);
+                res.send("Data form API is invalid");
+            }
+        }else 
+        {
+            res.status(400)
+            res.send("No data found").end();
+            console.log("No data found in ")
+        }
+        
+    })
+    
+});
+
+// Gets a single record
+app.post("/GNIMaleSingleRecord", (req, res) =>
+{
+    const json = req.body;
+    const year = req.body[0].Year;
+    const country = req.body[0].Country;
+    const valid = ajv.validate(GNI_MaleFemale_schema_single_record, json);
+    const values = [year, country];
+
+    const query = "SELECT `?` FROM estimated_gni_male WHERE Country = ?";
+    if(valid)
+    {   
+        conn.query(query, values, function(err, result, fields)
+        {
+            if(err)throw err;
+            if(result.length > 0)
+            {
+                console.log(result);
+                res.status(200);
+                res.send(result);
+            }else
+            {
+                res.status(204) // -204 No Content
+                res.send("No results found with given year.");
+            }
+            
+        })  
+    }else
+    {
+        res.status(400);
+        res.send("JSON not valid")
     }
-    else 
+
+});
+// -Updating data from client
+app.put("/estimatedGNIMale/update", (req, res) =>
+{
+    const data = req.body;
+    const reference = req.body[0].Country;
+
+    const valid = ajv.validate(GNI_MaleFemale_schema, data)
+    
+    if(valid)
+    {
+        console.log(data);
+        res.status(200)
+        res.send("Updated data");
+    }else
     {
         res.status(400)
-        res.send("DATA NOT BEEN INSERTED");
+        res.send("JSON invalid");
     }
-    console.log("asdasdad")
     
+});
+
+// -Selecting all data from table.
+// - This endpoint gets all the record that are the table:
+// # estimated_gni_female
+
+app.get("/estimatedGNIFemale", (req, res)=>
+{
+    const query = "SELECT * FROM estimated_gni_Female"
+    
+    conn.query(query, function(err, result, fields)
+    {
+        if(err) throw err;
+
+        const valid = ajv.validate(schema, result); 
+
+        // -checks if JSON data is valid with the schema
+        if(valid)
+        {
+            res.status(200);
+            res.send(result);
+        }else
+        {
+            res.status(400);
+            res.send("Data form API is invalid");
+        }
+    });
 });
 // -returns object with JSON
 app.get('/select', (req,res) =>
 {
-    var query = "SELECT estimated_gni_male.2000, estimated_gni_female.2000 FROM estimated_gni_male JOIN estimated_gni_female ON estimated_gni_male.Country = estimated_gni_female.Country WHERE estimated_gni_male.Country = 'Albania'";
-    conn.connect(function(err)
-    {
-        if(err) throw err;
-        conn.query(query, function (err, result, fields)
-        {
-            if(err)throw err;
-            res.send(result)
-        });
-    })
-});
+    var query = "";
 
+});
 
 
 app.listen(3000);
